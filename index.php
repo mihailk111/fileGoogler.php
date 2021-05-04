954597076678fd3286d313b991adb03d84fb41b8
Mikhail <you@example.com>
2021-05-05 02:36:14 +0500

    upgrade

--- index.php
+++ index.php
@@ -1,105 +1,15 @@
 <?php
-require __DIR__."/vendor/autoload.php";
 
-use Symfony\Component\Console\Helper\ProgressBar;
-use Symfony\Component\Console\Output\StreamOutput;
+require("./fileGoogler.php");
 
-use PHPHtmlParser\Dom;
-
-class fileGoogler
-{
-    private $search_expression = "password";
-    private $fileType = "txt";
-
-    public function __construct($expr, $ftype)
-    {
-        $this->fileType = $ftype;
-        $this->search_expression = $expr;
-        $this->domParser = new Dom();
-    }
-
-    public function find($pages_amount)
-    {
-        // $pb = new ProgressBar(new StreamOutput(STDOUT), $pages_amount * 10);        
-        // $pb->start();
-
-        $url = "https://google.com/search?q={$this->search_expression}+filetype:{$this->fileType}&client=ubuntu";
-
-        $all_files = [];
-
-        for ($i=0; $i < $pages_amount ; $i++) { 
-            echo "url -> $url\n";
-            $result = file_get_contents($url);
-            $links = $this->pageLinks($result); 
-            // for ($i=0; $i < count($links); $i++) { 
-            //     // $pb->advance(); 
-            // }
-            array_push($all_files, $links);
-
-            $url = $this->nextUrl($result); 
-
-            // file_put_contents("test.html", file_get_contents($url));
-            // die;
-            // try {
-            //     $url = $this->nextUrl($result); 
-            // } catch (Exception $e) {
-            //     if ($e->getCode() === 10) break;
-            // }
-        }
-
-        // $pb->finish();
-        // return array_merge(... $all_files) ;
-        return $all_files;
-        
-    }
-
-    private function pageLinks(string $html) : array
-    {
-
-        $dom = $this->domParser; 
-        $dom->loadStr($html);
-        $mainLinks = $dom->find("#main div div div a");
-
-        $array = [];
-        foreach ($mainLinks as $item) { $array []= $item->href; } 
-        $mainLinks = $array;
-
-        $mainLinks = array_filter($mainLinks, function($elem) {
-            if (str_contains($elem, '/url')) return $elem; 
-        });
-        $mainLinks = array_unique($mainLinks);
-
-        $mainLinks = array_map(function($elem){
-            preg_match('/\/url\?q=(.+?)\&amp;/', $elem, $match); // &amp; shows the end of the url
-            return $match[1] ?? "NOTHING";
-        }, $mainLinks);    
-
-        return $mainLinks;
-    }
-
-    private function nextUrl(string $html) : string
-    {
-        $dom = $this->domParser;
-        $dom->loadStr($html);
-        $data = $dom->find('footer div div div a');
-
-        // if (!isset($data[count($data)-1]->href)) {
-        //     throw new Exception("lastPage", 10);
-        // }
-        
-        $next_url = "https://google.com" . $data[count($data)-1]->href;
-        // dd($next_url);
-        return str_replace('&amp;', '&', $next_url);;
-    }
-}
 
 $google = new fileGoogler("passwords", "xlsx");
-print_r($google->find(10));
+
+print_r($google->find(3));
+
 
 function dd($in)
 {
     var_dump($in);
     die;
 }
-
-//FIXME find bugs
